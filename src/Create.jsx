ffb8373573ce116cf4fc3f06906a631c88e8ef4d
josh-hsiehh <joshua.hsieh@sjsu.edu>
2024-11-13 12:43:26 -0800

    to do list add button

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,18 +1,25 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 
-function Create() {
+function Create({ onTaskAdd }) {
   const[task, setTask] = useState("")
   
   const handleAdd = () => {
+    if (!task) return;
     axios.post("http://localhost:3001/add", {task: task})
-      .then(result => console.log(result))
+      .then(result => {
+        console.log(result);
+        setTask(""); // Clear input
+        onTaskAdd(); // Notify parent component
+      })
       .catch(err => console.log(err))
   }
   
   return (
     <div className="create_form">
-        <input type="text" placeholder='Enter Task' 
+        <input type="text" 
+          value={task}
+          placeholder='Enter Task' 
           onChange={(e) => setTask(e.target.value)}/>
         <button type="button" onClick={handleAdd}>Add</button>
     </div>
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,25 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Create from './Create'
 
 const Home = () => {
-    const[todos, setTodos]=useState([])
-  return (
-    <div className="home">
-      <h2>Todo List</h2>
-      <Create />
-      {
-        todos.length===0 
-        ?
-        <div>No Record </div>
-        :
-        todos.map(todo => (
-          <div>
-            {todo}
-          </div>
-        ))
-      }
-    </div>
-  )
+    const[todos, setTodos] = useState([])
+
+    const fetchTodos = () => {
+        axios.get("http://localhost:3001/get")
+            .then(result => setTodos(result.data))
+            .catch(err => console.log(err))
+    }
+
+    useEffect(() => {
+        fetchTodos()
+    }, [])
+
+    return (
+        <div className="home">
+            <h2>Todo List</h2>
+            <Create onTaskAdd={fetchTodos} />
+            {
+                todos.length === 0 
+                ?
+                <div>No Record</div>
+                :
+                todos.map(todo => (
+                    <div key={todo._id}>
+                        {todo.task}
+                    </div>
+                ))
+            }
+        </div>
+    )
 }
 
 
